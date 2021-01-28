70645f0810dbff0be47e6fbbc54860af4bd6653f
v.loitp <v.Loitp@cloudhms.net>
2021-01-28 16:30:41 +0700

    update

--- app/src/main/java/com/loitp/ui/activity/ReadNewsActivity.kt
+++ app/src/main/java/com/loitp/ui/activity/ReadNewsActivity.kt
@@ -2,12 +2,13 @@ package com.loitp.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
-import com.core.base.BaseApplication
 import com.core.base.BaseFontActivity
 import com.core.common.Constants
+import com.core.utilities.LAppResource
 import com.core.utilities.LImageUtil
 import com.loitp.R
 import com.rss.RssItem
@@ -45,12 +46,23 @@ class ReadNewsActivity : BaseFontActivity() {
         super.onCreate(savedInstanceState)
 
         rssItem = intent?.getSerializableExtra(KEY_RSS_ITEM) as RssItem?
-        logD("onCreate rssItem " + BaseApplication.gson.toJson(rssItem))
+//        logD("onCreate rssItem " + BaseApplication.gson.toJson(rssItem))
 
+        setupViews()
+    }
+
+    private fun setupViews() {
         rssItem?.let { item ->
-            LImageUtil.load(context = this, any = item.image, imageView = ivProfileDetailBackground)
-            tvDetailTitle.text = item.title
-            tvDetailDescription.text = item.description
+            LImageUtil.load(context = this, any = item.image, imageView = ivBkg)
+            collapsingToolbarLayout.title = item.title
         }
+
+        collapsingToolbarLayout.setExpandedTitleColor(Color.WHITE)
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE)
+
+//        toolbar.navigationIcon = LAppResource.getDrawable(R.drawable.ic_west_white_24dp)
+//        toolbar.setNavigationOnClickListener {
+//            onBackPressed()
+//        }
     }
 }
