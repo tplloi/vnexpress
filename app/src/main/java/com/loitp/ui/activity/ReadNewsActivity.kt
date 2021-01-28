d75b8f9a105a8cb680b59db1fd1ddb3fb50b7416
v.loitp <v.Loitp@cloudhms.net>
2021-01-28 17:02:51 +0700

    update

--- app/src/main/java/com/loitp/ui/activity/ReadNewsActivity.kt
+++ app/src/main/java/com/loitp/ui/activity/ReadNewsActivity.kt
@@ -4,17 +4,21 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import com.annotation.IsFullScreen
 import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.common.Constants
 import com.core.utilities.LAppResource
 import com.core.utilities.LImageUtil
+import com.core.utilities.LUIUtil
 import com.loitp.R
 import com.rss.RssItem
 import com.skydoves.transformationlayout.TransformationCompat
 import com.skydoves.transformationlayout.TransformationLayout
 import com.skydoves.transformationlayout.onTransformationEndContainer
+import com.views.LWebView
 import kotlinx.android.synthetic.main.activity_layout_read_news.*
 
 @LogTag("loitppReadNewsActivity")
@@ -64,5 +68,48 @@ class ReadNewsActivity : BaseFontActivity() {
         toolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        val isDarkMode = LUIUtil.isDarkTheme()
+        try {
+            webView.setDarkMode(isDarkMode)
+        } catch (e: Exception) {
+            showSnackBarError("setOnCheckedChangeListener $e")
+        }
+        webView.callback = object : LWebView.Callback {
+            override fun onScroll(l: Int, t: Int, oldl: Int, oldt: Int) {
+            }
+
+            override fun onScrollTopToBottom() {
+                logD("onScrollTopToBottom")
+            }
+
+            override fun onScrollBottomToTop() {
+                logD("onScrollBottomToTop")
+            }
+
+            override fun onProgressChanged(progress: Int) {
+                logD("onProgressChanged $progress")
+                pb.progress = progress
+                if (progress == 100) {
+                    pb.visibility = View.GONE
+                } else {
+                    pb.visibility = View.VISIBLE
+                }
+            }
+
+            override fun shouldOverrideUrlLoading(url: String) {
+            }
+        }
+        rssItem?.link?.let {
+            webView.loadUrl(it)
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
