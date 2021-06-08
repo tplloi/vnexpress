7c908c20d365114c0bc27769fe20e21b082c4e0e
Loitp <v.loitp@cloudhms.net>
2021-06-08 15:23:39 +0700

    update

--- app/src/main/java/com/loitp/ui/activity/GiftActivity.kt
+++ app/src/main/java/com/loitp/ui/activity/GiftActivity.kt
@@ -10,6 +10,7 @@ import com.annotation.LogTag
 import com.core.base.BaseFontActivity
 import com.core.common.Constants
 import com.core.utilities.LAppResource
+import com.core.utilities.LDialogUtil
 import com.core.utilities.LUIUtil
 import com.loitp.R
 import com.loitp.constant.Cons
@@ -26,6 +27,10 @@ import java.math.BigDecimal
 class GiftActivity : BaseFontActivity() {
 
     companion object {
+        const val CARD_TYPE_50 = "CARD_TYPE_50"
+        const val CARD_TYPE_200 = "CARD_TYPE_200"
+        const val CARD_TYPE_500 = "CARD_TYPE_500"
+
         fun startActivity(
             context: Context,
             transformationLayout: TransformationLayout
@@ -56,46 +61,75 @@ class GiftActivity : BaseFontActivity() {
         tvInformation.text = Cons.getCurrentMoneyInString()
 
         LUIUtil.setSafeOnClickListenerElastic(view = btViettel50, runnable = Runnable {
-            handleChange()
+            handleChange(CARD_TYPE_50)
         })
         LUIUtil.setSafeOnClickListenerElastic(view = btViettel200, runnable = Runnable {
-            handleChange()
+            handleChange(CARD_TYPE_200)
         })
         LUIUtil.setSafeOnClickListenerElastic(view = btViettel500, runnable = Runnable {
-            handleChange()
+            handleChange(CARD_TYPE_500)
         })
 
         LUIUtil.setSafeOnClickListenerElastic(view = btMobifone50, runnable = Runnable {
-            handleChange()
+            handleChange(CARD_TYPE_50)
         })
         LUIUtil.setSafeOnClickListenerElastic(view = btMobifone200, runnable = Runnable {
-            handleChange()
+            handleChange(CARD_TYPE_200)
         })
         LUIUtil.setSafeOnClickListenerElastic(view = btMobifone500, runnable = Runnable {
-            handleChange()
+            handleChange(CARD_TYPE_500)
         })
 
         LUIUtil.setSafeOnClickListenerElastic(view = btVinaphone50, runnable = Runnable {
-            handleChange()
+            handleChange(CARD_TYPE_50)
         })
         LUIUtil.setSafeOnClickListenerElastic(view = btVinaphone200, runnable = Runnable {
-            handleChange()
+            handleChange(CARD_TYPE_200)
         })
         LUIUtil.setSafeOnClickListenerElastic(view = btVinaphone500, runnable = Runnable {
-            handleChange()
+            handleChange(CARD_TYPE_500)
         })
     }
 
-    private fun handleChange() {
+    private fun handleChange(cardType: String) {
         val currentMoney = Cons.getCurrentMoneyInBigDecimal()
         if (currentMoney < BigDecimal(50000)) {
             showShortError(getString(R.string.not_valid_money))
-        } else {
-            showDialogProgress()
-            LUIUtil.setDelay(30000, Runnable {
-                hideDialogProgress()
-                showShortError(getString(R.string.cannot_change_money))
-            })
+            return
         }
+        when (cardType) {
+            CARD_TYPE_50 -> {
+                if (currentMoney >= BigDecimal(50000)) {
+                    LDialogUtil.showDialog2(
+                        context = this,
+                        title = getString(
+                            R.string.warning_vn
+                        ),
+                        msg = "Bạn có muốn đổi thẻ cào 50.000VND không?",
+                        button1 = getString(R.string.yes),
+                        button2 = getString(R.string.no),
+                        onClickButton1 = {
+                            showDialogProgress()
+                            LUIUtil.setDelay(3000, Runnable {
+                                hideDialogProgress()
+                                showShortError("Đã hết thẻ cào mệnh giá 50.000, vui lòng chọn phần thưởng khác")
+                            })
+                        },
+                        onClickButton2 = {
+                            //do nothing
+                        }
+                    )
+                } else {
+                    showShortError(getString(R.string.not_valid_money))
+                }
+            }
+            CARD_TYPE_200 -> {
+
+            }
+            CARD_TYPE_500 -> {
+
+            }
+        }
+
     }
 }
